60be0cfedf939ae56a4cfde2df70468c338c0c3a
Austin Luft <ausluft@gmail.com>
2018-09-25 05:52:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |attribute, value|
-      puts attribute.to_s.capitalize + ": " + value.join(" ")
+      puts attribute.to_s.capitalize + ": " + value.join(", ")
     end
   end
 end
